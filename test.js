4d2ba3201e4cf19cf1cbcd4528d94644030b5593
John Kavanagh (Digital) <john.kavanagh@lloydsbanking.com>
2019-05-14 17:23:23 +0100

    move search to its own file

--- test.js
+++ test.js
@@ -1,39 +1,9 @@
+const search = require('./search.js')
+
 const dfsGraph = require('./dfs.stub.json.js')
 const hlessGraph = require('./hless.stub.json.js')
-
-const search = (graph=[],target='',getAdjacentNodes=async()=>[]) => {  //TODO get/set reverse flag via method
-  let found = false
-
-  let stack = graph instanceof Array ? graph.slice().reverse() : [],  //TODO stack as Set?
-      visited = []
-
-  while(stack.length && !found) {
-
-    const pop = stack => {return {next:stack.slice(-1)[0],stack:stack.slice(0,-1)}}
-    const getNode = (items,obj) => obj instanceof Object ? Object[items](obj)[0] : null // TODO node as Weakmap?
-
-    let next
-    ({next,stack} = pop(stack))
-
-    let node = getNode('keys',next),
-        adjacentNodes = getAdjacentNodes(stack.slice(),visited.slice()) //TODO heuristic, async by default?
-
-    visited = [...visited,...(node?node:[])]
-
-    found = node && node===target
-
-    stack = [
-      ...(adjacentNodes && adjacentNodes.length ? getNode(adjacentNodes,'values') : []),
-      ...stack
-    ]
-
-  }
-  return visited
-}
-
-const dfs = (queue,visited) => {}
-
 const chai = require('chai');
+
 chai.should()
 chai.use(require('chai-fuzzy'));
 describe('Heuristicless search', function(){
