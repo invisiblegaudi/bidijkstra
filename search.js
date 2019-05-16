137d23b14ffb23c4362a7f095e65b906dc423074
John Kavanagh (Digital) <john.kavanagh@lloydsbanking.com>
2019-05-17 00:18:44 +0100

    clean up

--- search.js
+++ search.js
@@ -1,35 +1,32 @@
 const {getNode,getChildren} = require('./graph.js')
 
-const search = (graph=[],target='',getAdjacentNodes=async(next,stack,visited)=>[]) => {  //TODO get/set reverse flag via method
+const search = (graph=[],target='',getAdjacentNodes=async(next,stack,visited)=>[]) => {
+  //TODO search direction?
 
-  let found = false
+  let found = false,
+      stack = graph instanceof Array ? graph.slice() : [],  //TODO stack as Set?
+      visited = [] //TODO visited as Set?
 
-  let stack = graph instanceof Array ? graph.slice() : [],  //TODO stack as Set?
-      visited = []
+  const pop = stack => { return { next:stack.slice(0,1)[0],stack:stack.slice(1) } }  // TODO make generic
 
-  let i=0//TODO remove this
+  while(stack.length && !found) {
 
-  while(stack.length && !found
-        // && i<5
-       ) {
-    i++
-    const pop = stack => {return {next:stack.slice(0,1)[0],stack:stack.slice(1)}}
-    // TODO node as Weakmap?
-
-    let next
-    ({next,stack} = pop(stack))
+    let next; ({next,stack} = pop(stack))
 
     let node = getNode(next),
-        adjacentNodes = getAdjacentNodes(next,stack.slice(),visited.slice()) //TODO heuristic, async by default?
+        adjacentNodes = getAdjacentNodes(next,stack.slice(),visited.slice())
+    //TODO heuristic, async by default?
 
     visited = [...visited,...(node ? node : [])]
 
     found = node && node===target
 
-    stack = [...(adjacentNodes && adjacentNodes.length ? adjacentNodes : stack),]
+    stack = [...(adjacentNodes && adjacentNodes.length ? adjacentNodes : stack)]
 
   }
+
   return visited
+
 }
 
 module.exports = search
