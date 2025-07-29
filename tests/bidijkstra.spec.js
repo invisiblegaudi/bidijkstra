266eda2ba2e9256e2a3a217ea641623fa8001b6e
John Kavanagh <john@invisiblearchitects.com>
2025-07-29 15:14:18 +0100

    feat: add TypeScript configuration and migration for initial project setup
    refactor: migrate tests to Node native test runner
    
    update eslint
    
    chore: ignore all dot files with specific exceptions in .gitignore

--- tests/bidijkstra.spec.js
+++ tests/bidijkstra.spec.js
@@ -1,14 +1,12 @@
-const chai = require('chai')
+const { test, describe } = require('node:test')
+const assert = require('node:assert')
 const {arrAtoZ} = require('./mocks/ranges')
 const {findAtoZ} = require('../examples')
-const should = chai.should()
-
-should.should.have.property('fail')
 
 describe('Bi-directional Dijkstra search', () => {
 
-    it('returns a path equal to the alphabet', async () => {
+    test('returns a path equal to the alphabet', async () => {
         const path = await findAtoZ()
-        path.should.be.like(arrAtoZ)
+        assert.deepStrictEqual(path, arrAtoZ)
     })
 })
