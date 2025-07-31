f03fa5908d75d6e20ef3997b3d713f5b98d239ca
John Kavanagh <john@invisiblearchitects.com>
2025-07-31 16:13:08 +0100

    update output text

--- examples.js
+++ examples.js
@@ -2,7 +2,7 @@ const bidijkstra = require('./bidijkstra')
 
 const findAtoZ = async () => {
     const path = await bidijkstra('z','graphBFS','charDist','a','graphBFSreverse','charDistRev')
-    console.log('Match found! Result of both paths convererging:', path)
+  console.log('The paths have converged! Here is the combined path:', path)
     return path
 }
 
