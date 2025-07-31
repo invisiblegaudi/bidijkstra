debc09bd2a225997643525055b1549f6c8d78d18
John Kavanagh <john@invisiblearchitects.com>
2025-07-31 16:11:24 +0100

    test husky hooks

--- examples.js
+++ examples.js
@@ -3,7 +3,6 @@ const bidijkstra = require('./bidijkstra')
 const findAtoZ = async () => {
     const path = await bidijkstra('z','graphBFS','charDist','a','graphBFSreverse','charDistRev')
     console.log('Match found! Result of both paths convererging:', path)
-
     return path
 }
 
